c0b830344214068d8ef83a794f6ec561be8576b5
Jonas Andersen <94622735+jonasanders1@users.noreply.github.com>
2023-08-18 20:31:51 +0200

    dd

--- Card-App/ViewController.swift
+++ Card-App/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello")
+        print("Hello?")
         // Do any additional setup after loading the view.
     }
 
